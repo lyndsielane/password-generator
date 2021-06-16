c695976beda3bda5a2e80a3b7d8152cf643f1598
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-16 17:28:50 -0400

    Start

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+var specChar = (" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
+var options = [lowerCase, upperCase, numbers, specChar];
+var password = ""
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +17,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
