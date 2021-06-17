1a934958d7b45ec85b0311e2d2dc2ebd414d2701
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-17 18:00:03 -0400

    Mostly functional

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-var specChar = (" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
-var options = [lowerCase, upperCase, numbers, specChar];
-var password = ""
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-  window.alert ("How many characters for your password?"); 
-    if (< 8) {
-      window.alert ("Must be between 8-128 characters");
-    }
-    else if (> 128) {
-      window.alert ("Must be between 8-128 characters");
-    }
-    else {
-      window.alert ("Would you like to include uppercase letters?");
-    }
-    else {
-      window.alert ("Would you like to include special characters?")
-    }
+function generatePassword() {
+  //var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+  //var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  //var specChar = (" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
+
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specChar = " `~!@#$%^&*()_+-={}|[]\\:\";'<>?,./";
+
+  var lengthOfPassword = 10; //TODO: prompt("How many");
+  var requireUpperCase = true; //TODO: confirm("upper");
+  var requireNumber = false; //TODO: confirm("number");
+  var requireSpecChar = true; //TODO: confirm("special chars");
+
+  var password = '';
+
+  if (requireUpperCase) 
+  {
+    var randomIndex = Math.floor(Math.random() * letters.length);
+    password += letters[randomIndex].toUpperCase();
+  }
+
+  if (requireNumber) {
+    var randomIndex = Math.floor(Math.random() * numbers.length);
+    password += numbers[randomIndex].toUpperCase();
+  }
+
+  if (requireSpecChar) {
+    var randomIndex = Math.floor(Math.random() * specChar.length);
+    password += specChar[randomIndex];
+  }
+
+  var remainingChars = lengthOfPassword - password.length;
+
+  for(var i = 1; i <= remainingChars; i++) 
+  {
+    var randomIndex = Math.floor(Math.random() * letters.length);
+    var letter = letters[randomIndex];
+    console.log(`index: ${randomIndex}; letter: ${letter}`);
+    password += letter;
+  }
+
+  //TODO: now randomize the password before returning.
+
+  //TODO: Possible improvement would be to allow any number of requirements and to a sanity check before returning to confirm that the requirements are met.
+
+  return password;
+}
