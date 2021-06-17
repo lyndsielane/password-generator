d7fa4147416f2832f6952d7e2819ce155754fcfc
Lyndsie Lane <lyndsielane@yahoo.com>
2021-06-17 18:55:52 -0400

    Added window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -12,19 +12,14 @@ function writePassword() {
 }
 
 function generatePassword() {
-  //var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  //var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-  //var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-  //var specChar = (" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
-
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
   var specChar = " `~!@#$%^&*()_+-={}|[]\\:\";'<>?,./";
 
-  var lengthOfPassword = 10; //TODO: prompt("How many");
-  var requireUpperCase = true; //TODO: confirm("upper");
-  var requireNumber = false; //TODO: confirm("number");
-  var requireSpecChar = true; //TODO: confirm("special chars");
+  var lengthOfPassword = window.prompt("How many characters would you like for your password (must be between 8-128)?");//TODO: can I add an if/else to refuse numbers outside of the required length?
+  var requireUpperCase = window.confirm("Would you like to include uppercase letters?");
+  var requireNumber = window.confirm("Would you like to include numbers?");
+  var requireSpecChar = window.confirm("Would you like to include special characters?");
 
   var password = '';
 
